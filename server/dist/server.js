b9e08a1573779a0a72c0fc40a3520844219e537a
Yukimotochern <shtomcom@gmail.com>
2022-02-19 15:03:48 +0800

    Initial project setup

---
+++ server/dist/server.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var dotenv_1 = __importDefault(require("dotenv"));
+var api_1 = __importDefault(require("./routes/api"));
+var app = express_1.default();
+// Load env vars
+dotenv_1.default.config({ path: './config/config.env' });
+// Init Middleware
+app.use(express_1.default.json({ limit: '999999MB' }));
+app.use('/api/v1', api_1.default);
+console.log('Hello World');
+app.listen(5000);
---
+++ server/src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import api_v1 from './routes/api'
+
+const app = express()
+
+// Load env vars
+dotenv.config({ path: './config/config.env' })
+
+// Init Middleware
+app.use(express.json({ limit: '999999MB' }))
+app.use('/api/v1', api_v1)
+
+console.log('Hello World')
+app.listen(5000)
