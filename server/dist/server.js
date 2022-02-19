6c77dbc93b187913b513bee2594ef177f7ddf68e
Bryan Lin <78909016+bryanlin16899@users.noreply.github.com>
2022-02-19 17:55:22 +0800

    Finish mongodb.ts without uri

--- server/dist/server.js
+++ server/dist/server.js
@@ -6,11 +6,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var dotenv_1 = __importDefault(require("dotenv"));
 var api_1 = __importDefault(require("./routes/api"));
+var mongodb_1 = __importDefault(require("./utils/mongodb"));
 var app = (0, express_1.default)();
 // Load env vars
 dotenv_1.default.config({ path: './config/config.env' });
+// Connect to MongoDB
+(0, mongodb_1.default)();
 // Init Middleware
 app.use(express_1.default.json({ limit: '999999MB' }));
 app.use('/api/v1', api_1.default);
-console.log('Hello World!');
 app.listen(5000);
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,18 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import api_v1 from './routes/api'
+import connectDB from './utils/mongodb'
 
 const app = express()
 
 // Load env vars
 dotenv.config({ path: './config/config.env' })
 
+// Connect to MongoDB
+connectDB()
+
 // Init Middleware
 app.use(express.json({ limit: '999999MB' }))
 app.use('/api/v1', api_v1)
 
-console.log('text')
-console.log('Hello World!')
 app.listen(5000)
