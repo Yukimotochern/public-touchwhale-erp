acf07e3e4bb804e9ed3f88250efee292388c92f8
Yukimoto <shtomcom@gmail.com>
2022-03-01 23:39:24 +0800

    solve social

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -6,29 +6,32 @@ import axios, { AxiosRequestConfig, AxiosRequestHeaders } from 'axios'
 
 let config: AxiosRequestConfig = {
   timeout: +process.env.REACT_APP_API_TIMEOUT,
-  baseURL: `${process.env.REACT_APP_URL}/api/v${process.env.REACT_APP_API_VERSION}/`,
+  baseURL: process.env.REACT_APP_URL,
   headers: { 'content-type': 'application/json' },
 }
 
 class api {
   static async get(url: string, cof: AxiosRequestConfig = {}) {
-    return axios.get(url, { ...config, ...cof })
+    return axios.get(this.apiUrl(url), { ...config, ...cof })
   }
 
   static async post(url: string, data: any, cof: AxiosRequestConfig = {}) {
-    return axios.post(url, data, { ...cof, ...config })
+    return axios.post(this.apiUrl(url), data, { ...cof, ...config })
   }
 
   static async put(url: string, data: any, cof: AxiosRequestConfig = {}) {
-    return axios.put(url, data, { ...cof, ...config })
+    return axios.put(this.apiUrl(url), data, { ...cof, ...config })
   }
 
   static async delete(url: string, data: any, headers: AxiosRequestHeaders) {
-    return axios.delete(url, {
+    return axios.delete(this.apiUrl(url), {
       ...config,
       data,
       headers: { 'content-type': 'application/json', ...headers },
     })
+  }
+  static apiUrl(url: string) {
+    return `/api/v${process.env.REACT_APP_API_VERSION}/${url}`
   }
 }
 
