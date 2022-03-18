e5d9d1643ff49d309e9bf3a5e0408a74513d7b53
Yukimoto <shtomcom@gmail.com>
2022-03-18 20:55:33 +0800

    a

--- server/dist/features/TwItem/twItemRoutes.js
+++ server/dist/features/TwItem/twItemRoutes.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var authMiddleware_1 = __importDefault(require("../../middlewares/authMiddleware"));
-var itemOwnerMiddleware_1 = __importDefault(require("../../middlewares/itemOwnerMiddleware"));
+// import itemOwnerMiddleware from '../../middlewares/itemOwnerMiddleware'  2022/3/18 deprecated
 var advancedResult_1 = __importDefault(require("../../middlewares/advancedResult"));
 var errorCatcher_1 = __importDefault(require("../../middlewares/errorCatcher"));
 var twItemController_1 = require("./twItemController");
@@ -14,14 +14,14 @@ var router = express_1.default.Router();
 // @todo twItem routes supposed to handle diff user access right.
 router
     .route('/')
-    .get([authMiddleware_1.default, (0, advancedResult_1.default)(twItemModel_1.TwItem, 'setOfElement')], (0, errorCatcher_1.default)(twItemController_1.getItems))
-    .post(authMiddleware_1.default, (0, errorCatcher_1.default)(twItemController_1.addItem));
+    .all(authMiddleware_1.default)
+    .get([(0, advancedResult_1.default)(twItemModel_1.TwItem, 'setOfElement')], (0, errorCatcher_1.default)(twItemController_1.getItems))
+    .post((0, errorCatcher_1.default)(twItemController_1.addItem));
 router
     .route('/:id')
-    .get([authMiddleware_1.default, itemOwnerMiddleware_1.default], (0, errorCatcher_1.default)(twItemController_1.getItem))
-    .put([authMiddleware_1.default, itemOwnerMiddleware_1.default], (0, errorCatcher_1.default)(twItemController_1.updateItem))
-    .delete([authMiddleware_1.default, itemOwnerMiddleware_1.default], (0, errorCatcher_1.default)(twItemController_1.deleteItem));
-router
-    .route('/uploadImage/:id')
-    .get([authMiddleware_1.default, itemOwnerMiddleware_1.default], (0, errorCatcher_1.default)(twItemController_1.getB2URL));
+    .all(authMiddleware_1.default)
+    .get((0, errorCatcher_1.default)(twItemController_1.getItem))
+    .put((0, errorCatcher_1.default)(twItemController_1.updateItem))
+    .delete((0, errorCatcher_1.default)(twItemController_1.deleteItem));
+router.route('/uploadImage/:id').get([authMiddleware_1.default], (0, errorCatcher_1.default)(twItemController_1.getB2URL));
 exports.default = router;
