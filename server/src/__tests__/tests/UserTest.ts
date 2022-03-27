02a518544bc520174b92fa4a451ff6b1576f537f
Yukimoto <shtomcom@gmail.com>
2022-03-27 22:00:53 +0800

    before merge

--- server/src/__tests__/tests/UserTest.ts
+++ server/src/__tests__/tests/UserTest.ts
@@ -15,249 +15,249 @@ let getUploadAvatar: any
 const RequestServer = request(app)
 
 export const User_Test = () =>
-	describe('Test all user routes.', () => {
-		it('POST SignUp User', async () => {
-			const res = await RequestServer.post('/api/v1/user/signUp')
-				.send({
-					email: 'testEmail@gmail.com',
-				})
-				.set('Accept', 'application/json')
+  describe('Test all user routes.', () => {
+    it('POST SignUp User', async () => {
+      const res = await RequestServer.post('/api/v1/user/signUp')
+        .send({
+          email: 'testEmail@gmail.com',
+        })
+        .set('Accept', 'application/json')
 
-			expect(res.status).toBe(200)
-			expect(typeof res.body.message).toBe('string')
-		})
+      expect(res.status).toBe(200)
+      expect(typeof res.body.message).toBe('string')
+    })
 
-		it('Get verification code from mailtrap...', async () => {
-			const options = {
-				headers: { 'Api-Token': '8dc36cac15386caa83a61fdaeced2cb8' },
-			}
+    it('Get verification code from mailtrap...', async () => {
+      const options = {
+        headers: { 'Api-Token': '8dc36cac15386caa83a61fdaeced2cb8' },
+      }
 
-			// await fetch('https://mailtrap.io/api/v1/user', options)
-			const res_inbox = await axios.get(
-				'https://mailtrap.io/api/v1/inboxes/1616396/messages',
-				options
-			)
-			const mail_id = res_inbox.data[0]['id']
+      // await fetch('https://mailtrap.io/api/v1/user', options)
+      const res_inbox = await axios.get(
+        'https://mailtrap.io/api/v1/inboxes/1616396/messages',
+        options
+      )
+      const mail_id = res_inbox.data[0]['id']
 
-			const res_mail = await axios.get(
-				`https://mailtrap.io/api/v1/inboxes/1616396/messages/${mail_id}/body.txt`,
-				options
-			)
-			const data = res_mail.data.split(' ')
-			verification_code = data[data.length - 1].split('\n')[0]
+      const res_mail = await axios.get(
+        `https://mailtrap.io/api/v1/inboxes/1616396/messages/${mail_id}/body.txt`,
+        options
+      )
+      const data = res_mail.data.split(' ')
+      verification_code = data[data.length - 1].split('\n')[0]
 
-			console.log(
-				'****'.bgBlue,
-				'Verification code:',
-				verification_code,
-				'****'.bgBlue
-			)
-		})
+      console.log(
+        '****'.bgBlue,
+        'Verification code:',
+        verification_code,
+        '****'.bgBlue
+      )
+    })
 
-		it('POST Verify User', async () => {
-			const res = await RequestServer.post('/api/v1/user/signUp/verify')
-				.send({
-					email: 'testEmail@gmail.com',
-					password: verification_code,
-				})
-				.set('Accept', 'application/json')
+    it('POST Verify User', async () => {
+      const res = await RequestServer.post('/api/v1/user/signUp/verify')
+        .send({
+          email: 'testEmail@gmail.com',
+          password: verification_code,
+        })
+        .set('Accept', 'application/json')
 
-			expect(res.status).toBe(200)
-			expect(typeof res.body.data).toBe('string')
-			token = res.body.data
-		})
+      expect(res.status).toBe(200)
+      expect(typeof res.body.data).toBe('string')
+      token = res.body.data
+    })
 
-		it('PUT Set Password', async () => {
-			const res = await RequestServer.put('/api/v1/user/changePassword')
-				.set('Cookie', [`token=${token}`])
-				.send({
-					newPassword: 'test1234',
-				})
+    it('PUT Set Password', async () => {
+      const res = await RequestServer.put('/api/v1/user/changePassword')
+        .set('Cookie', [`token=${token}`])
+        .send({
+          newPassword: 'test1234',
+        })
 
-			expect(res.status).toBe(200)
-		})
+      expect(res.status).toBe(200)
+    })
 
-		it('POST User SignIn', async () => {
-			const res = await RequestServer.post('/api/v1/user/signIn')
-				.send({
-					email: 'testEmail@gmail.com',
-					password: 'test1234',
-				})
-				.set('Accept', 'application/json')
+    it('POST User SignIn', async () => {
+      const res = await RequestServer.post('/api/v1/user/signIn')
+        .send({
+          email: 'testEmail@gmail.com',
+          password: 'test1234',
+        })
+        .set('Accept', 'application/json')
 
-			expect(res.status).toBe(200)
-		})
+      expect(res.status).toBe(200)
+    })
 
-		it('GET Get User infomation', async () => {
-			const res = await RequestServer.get('/api/v1/user').set('Cookie', [
-				`token=${token}`,
-			])
+    it('GET Get User infomation', async () => {
+      const res = await RequestServer.get('/api/v1/user').set('Cookie', [
+        `token=${token}`,
+      ])
 
-			expect(res.status).toBe(200)
-			expect(typeof res.body).toBe('object')
-		})
+      expect(res.status).toBe(200)
+      expect(typeof res.body).toBe('object')
+    })
 
-		it('PUT Update User', async () => {
-			const res = await RequestServer.put('/api/v1/user')
-				.send({
-					username: 'testAccount',
-					company: 'FacGooAmazFilx',
-				})
-				.set('Accept', 'application/json')
-				.set('Cookie', [`token=${token}`])
+    it('PUT Update User', async () => {
+      const res = await RequestServer.put('/api/v1/user')
+        .send({
+          username: 'testAccount',
+          company: 'FacGooAmazFilx',
+        })
+        .set('Accept', 'application/json')
+        .set('Cookie', [`token=${token}`])
 
-			expect(res.status).toBe(200)
-			expect(typeof res.body).toBe('object')
-			// expect(console.log(res))
-		})
+      expect(res.status).toBe(200)
+      expect(typeof res.body).toBe('object')
+      // expect(console.log(res))
+    })
 
-		it('GET Upload Avatar URL', async () => {
-			const res = await RequestServer.get('/api/v1/user/avatar').set('Cookie', [
-				`token=${token}`,
-			])
+    it('GET Upload Avatar URL', async () => {
+      const res = await RequestServer.get('/api/v1/user/avatar').set('Cookie', [
+        `token=${token}`,
+      ])
 
-			expect(res.status).toBe(200)
-			expect(typeof res.body).toBe('object')
+      expect(res.status).toBe(200)
+      expect(typeof res.body).toBe('object')
 
-			getUploadAvatar = res.body.data
-		})
+      getUploadAvatar = res.body.data
+    })
 
-		it('Upload Avatar to B2 ...', async () => {
-			const { uploadUrl, avatar } = getUploadAvatar
-			let image = fs.createReadStream(
-				'./src/__tests__/public/testAvatar.png'
-			) as any
-			let data = image.toString()
-			const formData = new FormData()
+    it('Upload Avatar to B2 ...', async () => {
+      const { uploadUrl, avatar } = getUploadAvatar
+      let image = fs.createReadStream(
+        './src/__tests__/public/testAvatar.png'
+      ) as any
+      let data = image.toString()
+      const formData = new FormData()
 
-			formData.append('image', image)
+      formData.append('image', image)
 
-			await axios.put(
-				uploadUrl,
-				{ data: Buffer.from(data, 'binary') },
-				{
-					withCredentials: true,
-					headers: { 'Content-Type': 'image/jpeg' },
-				}
-			)
-		})
+      await axios.put(
+        uploadUrl,
+        { data: Buffer.from(data, 'binary') },
+        {
+          withCredentials: true,
+          headers: { 'Content-Type': 'image/jpeg' },
+        }
+      )
+    })
 
-		it('Delete Avatar', async () => {
-			const res = await RequestServer.delete('/api/v1/user/avatar').set(
-				'Cookie',
-				[`token=${token}`]
-			)
+    it('Delete Avatar', async () => {
+      const res = await RequestServer.delete('/api/v1/user/avatar').set(
+        'Cookie',
+        [`token=${token}`]
+      )
 
-			expect(res.status).toBe(200)
-			expect(res.body.message).toBe('Avatar deleted.')
-		})
+      expect(res.status).toBe(200)
+      expect(res.body.message).toBe('Avatar deleted.')
+    })
 
-		it('Change Password', async () => {
-			const res = await RequestServer.put('/api/v1/user/changePassword')
-				.send({
-					currentPassword: 'test1234',
-					newPassword: 'Test2345',
-				})
-				.set('Cookie', [`token=${token}`])
+    it('Change Password', async () => {
+      const res = await RequestServer.put('/api/v1/user/changePassword')
+        .send({
+          currentPassword: 'test1234',
+          newPassword: 'Test2345',
+        })
+        .set('Cookie', [`token=${token}`])
 
-			expect(res.status).toBe(200)
-		})
+      expect(res.status).toBe(200)
+    })
 
-		it('Forget Passwrod', async () => {
-			const res = await RequestServer.post('/api/v1/user/forgetPassword').send({
-				email: 'testEmail@gmail.com',
-			})
+    it('Forget Passwrod', async () => {
+      const res = await RequestServer.post('/api/v1/user/forgetPassword').send({
+        email: 'testEmail@gmail.com',
+      })
 
-			expect(res.status).toBe(200)
-		})
+      expect(res.status).toBe(200)
+    })
 
-		it('Get resetPassword url from mailtrap...', async () => {
-			const options = {
-				headers: { 'Api-Token': '8dc36cac15386caa83a61fdaeced2cb8' },
-			}
+    it('Get resetPassword url from mailtrap...', async () => {
+      const options = {
+        headers: { 'Api-Token': '8dc36cac15386caa83a61fdaeced2cb8' },
+      }
 
-			// await fetch('https://mailtrap.io/api/v1/user', options)
-			const res_inbox = await axios.get(
-				'https://mailtrap.io/api/v1/inboxes/1616396/messages',
-				options
-			)
-			const mail_id = res_inbox.data[0]['id']
+      // await fetch('https://mailtrap.io/api/v1/user', options)
+      const res_inbox = await axios.get(
+        'https://mailtrap.io/api/v1/inboxes/1616396/messages',
+        options
+      )
+      const mail_id = res_inbox.data[0]['id']
 
-			const res_mail = await axios.get(
-				`https://mailtrap.io/api/v1/inboxes/1616396/messages/${mail_id}/body.txt`,
-				options
-			)
-			const data = res_mail.data.split(' ')
-			resetPassword_url = data[data.length - 1].split('\n')[0]
-			resetPassword_token = resetPassword_url
-				.split('/')
-				[resetPassword_url.split('/').length - 1].split('#')[1]
+      const res_mail = await axios.get(
+        `https://mailtrap.io/api/v1/inboxes/1616396/messages/${mail_id}/body.txt`,
+        options
+      )
+      const data = res_mail.data.split(' ')
+      resetPassword_url = data[data.length - 1].split('\n')[0]
+      resetPassword_token = resetPassword_url
+        .split('/')
+        [resetPassword_url.split('/').length - 1].split('#')[1]
 
-			console.log(
-				'****'.bgBlue,
-				'Reset Password URL:',
-				resetPassword_url,
-				'Reset Password Token:',
-				resetPassword_token,
-				'****'.bgBlue
-			)
-		})
+      console.log(
+        '****'.bgBlue,
+        'Reset Password URL:',
+        resetPassword_url,
+        'Reset Password Token:',
+        resetPassword_token,
+        '****'.bgBlue
+      )
+    })
 
-		it('Reset Password First Call', async () => {
-			const res = await RequestServer.put('/api/v1/user/forgetPassword').send({
-				token: resetPassword_token,
-			})
+    it('Reset Password First Call', async () => {
+      const res = await RequestServer.put('/api/v1/user/forgetPassword').send({
+        token: resetPassword_token,
+      })
 
-			expect(res.status).toBe(200)
-			expect(typeof res.body.data.token).toBe('string')
+      expect(res.status).toBe(200)
+      expect(typeof res.body.data.token).toBe('string')
 
-			resetPassword_token = res.body.data.token
-			console.log(
-				'****'.bgBlue,
-				'New Reset Password Token:',
-				resetPassword_token,
-				'****'.bgBlue
-			)
-		})
+      resetPassword_token = res.body.data.token
+      console.log(
+        '****'.bgBlue,
+        'New Reset Password Token:',
+        resetPassword_token,
+        '****'.bgBlue
+      )
+    })
 
-		it('Resrt Password Second Call', async () => {
-			const res = await RequestServer.put('/api/v1/user/forgetPassword').send({
-				token: resetPassword_token,
-				password: 'test3456',
-			})
+    it('Resrt Password Second Call', async () => {
+      const res = await RequestServer.put('/api/v1/user/forgetPassword').send({
+        token: resetPassword_token,
+        password: 'test3456',
+      })
 
-			expect(res.status).toBe(200)
-			expect(res.body.message).toBe('Your password has been set.')
-		})
+      expect(res.status).toBe(200)
+      expect(res.body.message).toBe('Your password has been set.')
+    })
 
-		it('POST User SignIn After Resetpassword', async () => {
-			const res = await RequestServer.post('/api/v1/user/signIn')
-				.send({
-					email: 'testEmail@gmail.com',
-					password: 'test3456',
-				})
-				.set('Accept', 'application/json')
+    it('POST User SignIn After Resetpassword', async () => {
+      const res = await RequestServer.post('/api/v1/user/signIn')
+        .send({
+          email: 'testEmail@gmail.com',
+          password: 'test3456',
+        })
+        .set('Accept', 'application/json')
 
-			expect(res.status).toBe(200)
-			token = res.body
-		})
+      expect(res.status).toBe(200)
+      token = res.body
+    })
 
-		// it('POST Create Worker', async () => {
-		// 	const res = await request(app)
-		// 		.post('/api/v1/user/workers')
-		// 		.send({
-		// 			login_name: 'TestWorker',
-		// 			password: 'test1234',
-		// 			role_type: 'default',
-		// 			permission_groups: ['human resource'],
-		// 			provider: 'TouchWhale',
-		// 			isActice: true,
-		// 			isOwner: false,
-		// 		})
-		// 		.set('Accept', 'application/json')
-		// 		.set('Cookie', [`token=${token}`])
-		// 	console.log(res)
+    // it('POST Create Worker', async () => {
+    // 	const res = await request(app)
+    // 		.post('/api/v1/user/workers')
+    // 		.send({
+    // 			login_name: 'TestWorker',
+    // 			password: 'test1234',
+    // 			role_type: 'default',
+    // 			permission_groups: ['human resource'],
+    // 			provider: 'TouchWhale',
+    // 			isActice: true,
+    // 			isOwner: false,
+    // 		})
+    // 		.set('Accept', 'application/json')
+    // 		.set('Cookie', [`token=${token}`])
+    // 	console.log(res)
 
-		// 	expect(res.status).toBe(200)
-		// })
-	})
+    // 	expect(res.status).toBe(200)
+    // })
+  })
--- server/src/__tests__/tests/WorkerTest.ts
+++ server/src/__tests__/tests/WorkerTest.ts
@@ -8,128 +8,128 @@ let worker_data: any
 const RequestServer = request(app)
 
 export const Worker_Test = () => {
-	it('SignIn a admin user', async () => {
-		const res = await RequestServer.post('/api/v1/user/signIn')
-			.send({
-				email: 'testEmail@gmail.com',
-				password: 'test3456',
-			})
-			.set('Accept', 'application/json')
+  it('SignIn a admin user', async () => {
+    const res = await RequestServer.post('/api/v1/user/signIn')
+      .send({
+        email: 'testEmail@gmail.com',
+        password: 'test3456',
+      })
+      .set('Accept', 'application/json')
 
-		expect(res.status).toBe(200)
-		token = res.body
-	})
+    expect(res.status).toBe(200)
+    token = res.body
+  })
 
-	describe('Worker function', () => {
-		it('POST Create a worker', async () => {
-			const res = await RequestServer.post('/api/v1/user/workers')
-				.set('Cookie', [`token=${token}`])
-				.send({
-					login_name: 'testworker1234',
-					password: 'test1234',
-					permission_groups: ['admin'],
-					role_type: 'default',
-				})
-			expect(res.status).toBe(200)
-		})
+  describe('Worker function', () => {
+    it('POST Create a worker', async () => {
+      const res = await RequestServer.post('/api/v1/user/workers')
+        .set('Cookie', [`token=${token}`])
+        .send({
+          login_name: 'testworker1234',
+          password: 'test1234',
+          permission_groups: ['admin'],
+          role_type: 'default',
+        })
+      expect(res.status).toBe(200)
+    })
 
-		it('GET User signout', async () => {
-			const res = await RequestServer.get('/api/v1/user/signOut')
+    it('GET User signout', async () => {
+      const res = await RequestServer.get('/api/v1/user/signOut')
 
-			expect(res.status).toBe(200)
-		})
+      expect(res.status).toBe(200)
+    })
 
-		it('POST Worker signin', async () => {
-			const res = await RequestServer.post('/api/v1/user/signIn')
-				.send({
-					login_name: 'testworker1234',
-					password: 'test1234',
-				})
-				.set('Accept', 'application/json')
+    it('POST Worker signin', async () => {
+      const res = await RequestServer.post('/api/v1/user/signIn')
+        .send({
+          login_name: 'testworker1234',
+          password: 'test1234',
+        })
+        .set('Accept', 'application/json')
 
-			expect(res.status).toBe(200)
-		})
+      expect(res.status).toBe(200)
+    })
 
-		it('GET Worker signout', async () => {
-			const res = await RequestServer.get('/api/v1/user/signOut')
+    it('GET Worker signout', async () => {
+      const res = await RequestServer.get('/api/v1/user/signOut')
 
-			expect(res.status).toBe(200)
-		})
+      expect(res.status).toBe(200)
+    })
 
-		// it('POST Signin admin user', async () => {
-		// 	const res = await RequestServer.post('/api/v1/user/signIn')
-		// 		.send({
-		// 			email: 'testEmail@gmail.com',
-		// 			password: 'test3456',
-		// 		})
-		// 		.set('Accept', 'application/json')
+    // it('POST Signin admin user', async () => {
+    // 	const res = await RequestServer.post('/api/v1/user/signIn')
+    // 		.send({
+    // 			email: 'testEmail@gmail.com',
+    // 			password: 'test3456',
+    // 		})
+    // 		.set('Accept', 'application/json')
 
-		// 	expect(res.status).toBe(200)
-		// 	token = res.body
-		// })
+    // 	expect(res.status).toBe(200)
+    // 	token = res.body
+    // })
 
-		it('POST Create another worker', async () => {
-			const a = await RequestServer.post('/api/v1/user/signIn')
-				.send({
-					email: 'testEmail@gmail.com',
-					password: 'test3456',
-				})
-				.set('Accept', 'application/json')
+    it('POST Create another worker', async () => {
+      const a = await RequestServer.post('/api/v1/user/signIn')
+        .send({
+          email: 'testEmail@gmail.com',
+          password: 'test3456',
+        })
+        .set('Accept', 'application/json')
 
-			token = a.body
+      token = a.body
 
-			const res = await RequestServer.post('/api/v1/user/workers')
-				.set('Cookie', [`token=${token}`])
-				.send({
-					login_name: 'testworker2234',
-					password: 'test2234',
-					permission_groups: ['human resource'],
-					role_type: 'default',
-				})
-			expect(res.status).toBe(200)
-		})
+      const res = await RequestServer.post('/api/v1/user/workers')
+        .set('Cookie', [`token=${token}`])
+        .send({
+          login_name: 'testworker2234',
+          password: 'test2234',
+          permission_groups: ['human resource'],
+          role_type: 'default',
+        })
+      expect(res.status).toBe(200)
+    })
 
-		it('GET Get all workers', async () => {
-			const res = await RequestServer.get('/api/v1/user/workers').set(
-				'Cookie',
-				[`token=${token}`]
-			)
+    it('GET Get all workers', async () => {
+      const res = await RequestServer.get('/api/v1/user/workers').set(
+        'Cookie',
+        [`token=${token}`]
+      )
 
-			expect(res.status).toBe(200)
-			expect(typeof res.body.data).toBe('object')
-			worker_data = await UserModel.findOne({ isOwner: false })
-		})
+      expect(res.status).toBe(200)
+      expect(typeof res.body.data).toBe('object')
+      worker_data = await UserModel.findOne({ isOwner: false })
+    })
 
-		it('GET Get single worker', async () => {
-			const res = await RequestServer.get(
-				`/api/v1/user/workers/${worker_data._id}`
-			).set('Cookie', [`token=${token}`])
+    it('GET Get single worker', async () => {
+      const res = await RequestServer.get(
+        `/api/v1/user/workers/${worker_data._id}`
+      ).set('Cookie', [`token=${token}`])
 
-			expect(res.status).toBe(200)
-			expect(typeof res.body.data).toBe('object')
-		})
+      expect(res.status).toBe(200)
+      expect(typeof res.body.data).toBe('object')
+    })
 
-		it('PUT Update a worker', async () => {
-			const res = await RequestServer.put(
-				`/api/v1/user/workers/${worker_data._id}`
-			)
-				.set('Cookie', [`token=${token}`])
-				.send({
-					permission_groups: ['admin'],
-				})
+    it('PUT Update a worker', async () => {
+      const res = await RequestServer.put(
+        `/api/v1/user/workers/${worker_data._id}`
+      )
+        .set('Cookie', [`token=${token}`])
+        .send({
+          permission_groups: ['admin'],
+        })
 
-			expect(res.status).toBe(200)
-		})
+      expect(res.status).toBe(200)
+    })
 
-		it('DELETE Delete a worker', async () => {
-			const res = await RequestServer.delete(
-				`/api/v1/user/workers/${worker_data._id}`
-			).set('Cookie', [`token=${token}`])
+    it('DELETE Delete a worker', async () => {
+      const res = await RequestServer.delete(
+        `/api/v1/user/workers/${worker_data._id}`
+      ).set('Cookie', [`token=${token}`])
 
-			expect(res.status).toBe(200)
-			expect(res.body.message).toBe(
-				'The user in the data is successfully deleted.'
-			)
-		})
-	})
+      expect(res.status).toBe(200)
+      expect(res.body.message).toBe(
+        'The user in the data is successfully deleted.'
+      )
+    })
+  })
 }
